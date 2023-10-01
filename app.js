e457080385fc47168865f4af180dc12e270e126a
Santiago Orozco Buri <santiago.orozco@genoneclt.org>
2023-10-01 10:10:02 -0400

    built the express app

---
+++ app.js
@@ -0,0 +1,26 @@
+//require modules
+const express = require("express");
+const morgan = require("morgan");
+
+//create app
+const app = express();
+
+//config app
+let port = 3000;
+let host = "localhost";
+app.set("view engine", "ejs");
+
+//middleware functions
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+//setup routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//start server
+app.listen(port, host, () => {
+  console.log("Server is running on port", port);
+});
